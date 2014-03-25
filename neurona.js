bdeb7d73be3a8289064fae0a60d410897195b60e
Fernando Montoya <montogeek@gmail.com>
2014-03-25 15:11:47 -0500

    Better performance, little style change

--- neurona.js
+++ neurona.js
@@ -4,7 +4,7 @@ function fadeIn(el) {
 
     var last = +new Date(),
         tick = function () {
-            el.style.opacity = +el.style.opacity + (new Date() - last) / 400;
+            el.style.opacity = +el.style.opacity + (new Date() - last) / 1000;
             last = +new Date();
 
             if (+el.style.opacity < 1) {
@@ -14,25 +14,26 @@ function fadeIn(el) {
     tick();
 }
 
-function NumFilter (num) {
+function numFilter(num) {
+    'use strict';
     return num.toFixed(3);
 }
 
-function Sigmoide(s) {
+function sigmoide(s) {
     'use strict';
     return 1 / (1 + Math.pow(Math.E, -s));
 }
-function DeltaK(d, f) {
+function deltaK(d, f) {
     'use strict';
     return f * (d - f) * (1 - f);
 }
 
-function DeltaJ(deltak, peso, fj) {
+function deltaJ(deltak, peso, fj) {
     'use strict';
     return (1 - fj) * deltak * peso * fj;
 }
 
-function PesoK(valor, deltak, x) {
+function pesoK(valor, deltak, x) {
     'use strict';
     return valor + (deltak *  x);
 }
@@ -41,19 +42,17 @@ function neurona(event) {
     'use strict';
     event.preventDefault();
     // Entrada (X1, X2)
-    var entrada = [],
-        entrada1 = parseInt(document.getElementsByName('entrada1')[0].value, 10),
-        entrada2 = parseInt(document.getElementsByName('entrada2')[0].value, 10),
+    var entrada = Array.prototype.slice.call(document.getElementsByName('entrada'), 0).map(function(element) {
+           return parseInt(element.value, 10);
+        }),
     
     // Valor deseado de salida (d)
         valor_deseado = parseInt(document.getElementsByName('valor_deseado')[0].value, 10),
     
     // Pesos Unidades Sigmoidales
-        peso   = [],
-        peso1   = parseInt(document.getElementsByName('peso1')[0].value, 10),
-        peso2   = parseInt(document.getElementsByName('peso2')[0].value, 10),
-        peso3   = parseInt(document.getElementsByName('peso3')[0].value, 10),
-        peso4   = parseInt(document.getElementsByName('peso4')[0].value, 10),
+        peso    = Array.prototype.slice.call(document.getElementsByName('peso'), 0).map(function(element) {
+            return parseInt(element.value, 10);
+        }),
 
     // Otras variables
         deltak  = 0,
@@ -65,65 +64,59 @@ function neurona(event) {
         f       = [],   // Función Sigmoidal capa 2
         s       = 0;
     
-    entrada.push(entrada1, entrada2);
-    peso.push(peso1, peso2, peso3, peso4);
-    
     // Process
     // ----------------
 
-    //  Salida de las primeras neuronas utilizando la función Sigmoide(s).
+    //  Salida de las primeras neuronas utilizando la función sigmoide(s).
     //  El valor s es el producto de la acción (entrada1) por peso (peso1)
-    for ( i = 0; i < entrada.length; i++) {
-        s = entrada[i]*peso[i];
-        f_temp.push( Sigmoide(s) );
+    for (i = 0; i < entrada.length; i = i + 1) {
+        s = entrada[i] * peso[i];
+        f_temp.push(sigmoide(s));
     }
     
     //  Salida de la última neurona. Se obtiene de la resta 
     //  de la función sigmoide de los valores obtenidos anteriormente
     //  por su peso correspondiente
-    for ( i = 0; i < f_temp.length; i++ ) {
-        s = f_temp[i]*peso[i+2];
-        f.push( Sigmoide(s) );
+    for (i = 0; i < f_temp.length; i = i + 1) {
+        s = f_temp[i] * peso[i + 2];
+        f.push(sigmoide(s));
     }
-    f.push(f[0]-f[1]);
+    
+    f.push(f[0] - f[1]);
 
     //  Función Delta para la última capa (deltak)
-    deltak = DeltaK( valor_deseado, f[2] );
+    deltak = deltaK(valor_deseado, f[2]);
 
     //  Función Delta propagada hacia atrás con los pesos de la
     //  segunda capa (DeltaJ)
     deltaj.push(
-        DeltaJ( deltak, peso[2], f_temp[0] ),
-        DeltaJ( deltak, peso[3], f_temp[1] )
+        deltaJ(deltak, peso[2], f_temp[0]),
+        deltaJ(deltak, peso[3], f_temp[1])
     );
 
     // Cálculo de nuevos pesos
-    pesok.push(
-        PesoK( peso[0], deltak, c ),
-        PesoK( peso[1], deltak, c ),
-        PesoK( peso[2], deltak, c ),
-        PesoK( peso[3], deltak, c )
-    );
-
+    pesok = peso.map(function(value) {
+        return pesoK(value, deltak, c);
+    });
 
     // Output
     // -----------------
 
     //  Sigmoides
-    document.getElementById('sigmoide1').innerHTML = NumFilter(f_temp[0]);
-    document.getElementById('sigmoide2').innerHTML = NumFilter(f_temp[1]);
-    document.getElementById('sigmoide3').innerHTML = NumFilter(f[2]);
+    document.getElementById('sigmoide1').innerHTML = numFilter(f_temp[0]);
+    document.getElementById('sigmoide2').innerHTML = numFilter(f_temp[1]);
+    document.getElementById('sigmoide3').innerHTML = numFilter(f[2]);
 
     //  Deltas
-    document.getElementById('delta1').innerHTML = NumFilter(deltak);
-    document.getElementById('delta2').innerHTML = NumFilter(deltaj[0]);
-    document.getElementById('delta3').innerHTML = NumFilter(deltaj[1]);
+    document.getElementById('delta1').innerHTML = numFilter(deltak);
+    document.getElementById('delta2').innerHTML = numFilter(deltaj[0]);
+    document.getElementById('delta3').innerHTML = numFilter(deltaj[1]);
 
     //  Pesos
-    document.getElementById('peso1').innerHTML = NumFilter(pesok[0]);
-    document.getElementById('peso2').innerHTML = NumFilter(pesok[1]);
-    document.getElementById('peso3').innerHTML = NumFilter(pesok[2]);
-    document.getElementById('peso4').innerHTML = NumFilter(pesok[3]);
+    document.getElementById('peso1').innerHTML = numFilter(pesok[0]);
+    document.getElementById('peso2').innerHTML = numFilter(pesok[1]);
+    document.getElementById('peso3').innerHTML = numFilter(pesok[2]);
+    document.getElementById('peso4').innerHTML = numFilter(pesok[3]);
 
 
     // Fancy output
